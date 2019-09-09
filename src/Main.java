f4c37cfd83db464895cfe59e5523fd85ee0c2e38
Kamrul Alam <mebeshaon@gmail.com>
2019-09-09 17:27:09 -0400

    fixed origin issue

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,23 @@
 import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
-
 public class Main {
-	static long NW = 10;
+	static long NW = 1000000;
 	static int x = 0;
 	static int y = 0;
+	static int numSteps =3;
 	static String tempPosition = "";
 
 	public static void main(String[] args) {
 
 	for(int iwalk =0; iwalk< NW; iwalk++) {
 		ArrayList<String> myList = new ArrayList<String>();
+		x=0;
+		y=0;
 		String home = "(" + x + "," + y + ")";
 		System.out.println("pass "+ (iwalk+1));
 		myList.add(home);
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < numSteps; i++) {
 			nextStep();
 			if(myList.contains(tempPosition)) {
 				System.out.println("non-SAW");
